0823abb3ed6b5fa7a1a482a11bd84a636c1f1f9a
Drew Dunne <drewsdunne1@gmail.com>
2019-02-13 17:49:49 -0500

    initial app commit

---
+++ Chatty Cathy/Chatty Cathy/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Chatty Cathy
+//
+//  Created by Drew Dunne on 2/13/19.
+//  Copyright © 2019 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ Chatty Cathy/Pods/Target Support Files/Pods-Chatty Cathy/Pods-Chatty Cathy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Chatty_Cathy : NSObject
+@end
+@implementation PodsDummy_Pods_Chatty_Cathy
+@end
